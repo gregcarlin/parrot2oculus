57f5b23c3b3c6dc2fa9ea4abd8a199d1b402da05
Greg Carlin <gregcarlin1@gmail.com>
2015-09-19 13:51:44 -0400

    Initial Commit

---
+++ parrot2oculus/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  parrot2oculus
+//
+//  Created by Greg Carlin on 9/19/15.
+//  Copyright © 2015 Brown2HackMIT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ parrot2oculus/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  parrot2oculus
+//
+//  Created by Greg Carlin on 9/19/15.
+//  Copyright © 2015 Brown2HackMIT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
